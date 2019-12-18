4957c5bfa9e9cafb4ffc689b3322741c6ee46d2f
yangyxu <945758912@qq.com>
2019-12-18 19:44:33 +0800

    fix relation issue

--- build/index.js
+++ build/index.js
@@ -4,6 +4,7 @@ if (!znui) {
   require('znui-react/index.all.js');
 }
 
+znui.react.fixCreateReactClass(require('react'), require('create-react-class'));
 module.exports = {
   'Node': require('./Node'),
   'Link': require('./Link'),
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 if(!znui){
     require('znui-react/index.all.js');
 }
+znui.react.fixCreateReactClass(require('react'), require('create-react-class'));
 module.exports = {
     'Node': require('./Node'),
     'Link': require('./Link'),
